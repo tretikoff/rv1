254dfa081ece874dafda1f302b8322fac10c6940
konstantin <konstantin.tretikov@studiomobile.ru>
2019-03-14 13:25:47 +0300

    sdf

--- ipc.c
+++ ipc.c
@@ -1,25 +1,21 @@
 #include "common.h"
 #include "ipc.h"
 #include "pa1.h"
-#include <sys/types.h>
+
 #include <unistd.h>
 #include <stdio.h>
+
+#include <sys/types.h>
+#include <getopt.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/*
- * typedef struct {
-    uint16_t     s_magic;        ///< magic signature, must be MESSAGE_MAGIC
-    uint16_t     s_payload_len;  ///< length of payload
-    int16_t      s_type;         ///< type of the message
-    timestamp_t  s_local_time;   ///< set by sender, depends on particular PA:
-                                 ///< physical time in PA2 or Lamport's scalar
-                                 ///< time in PA3
-} __attribute__((packed)) MessageHeader;
- */
-
+void close_pipes(void *self, int proc);
+int usleep(__useconds_t usec);
 int receive_all(void *self, Message *msgs, MessageType type);
 
 void createMessageHeader(Message *msg, MessageType type);
@@ -36,7 +32,7 @@ typedef struct {
 
 typedef struct {
     InputOutput io;
-    int self;
+    local_id self;
 } SelfInputOutput;
 
 int main(int argc, char **argv) {
@@ -57,24 +53,27 @@ int main(int argc, char **argv) {
     io.fds = (int ***) calloc((proc_count + 1), sizeof(int **));
 
     FILE *pipes_logfile = fopen(pipes_log, "a+");
-    for (int i = 0; i <= proc_count + 1; ++i) {
+    for (int i = 0; i <= proc_count; ++i) {
         io.fds[i] = (int **) calloc((proc_count + 1), sizeof(int *));
-        for (int j = 0; j <= proc_count + 1; ++j) {
+        for (int j = 0; j <= proc_count; ++j) {
+            if (i == j) continue;
             io.fds[i][j] = (int *) calloc(2, sizeof(int));
             pipe(io.fds[i][j]);
             fprintf(pipes_logfile, "%d %d was opened\n", i, j);
         }
     }
 
+    FILE *logfile;
+    logfile = fopen(events_log, "a+");
+
     int pid = 0;
-    for (int i = 1; i <= proc_count; ++i) {
+    for (local_id i = 1; i <= proc_count; ++i) {
         pid = fork();
         if (pid == 0) {
             pids[i] = pid;
             SelfInputOutput sio = {io, i};
+            close_pipes(&sio, i);
 
-            FILE *logfile;
-            logfile = fopen(events_log, "a+");
             fprintf(logfile, log_started_fmt, i, getpid(), getppid());
             fflush(logfile);
 
@@ -92,26 +91,35 @@ int main(int argc, char **argv) {
             Message msg2;
             sprintf(msg2.s_payload, log_done_fmt, i);
             createMessageHeader(&msg2, DONE);
-            send_multicast(&sio, &msg2);
 
             fprintf(logfile, log_done_fmt, i);
+            send_multicast(&sio, &msg2);
 
             receive_all(&sio, msgs, DONE);
             fprintf(logfile, log_received_all_done_fmt, i);
 
-            exit(0);
-        } else {
-            SelfInputOutput sio = {io, 0};
-            Message msgs[proc_count + 1];
-            receive_all(&sio, msgs, STARTED);
-            receive_all(&sio, msgs, DONE);
+            return 0;
         }
     }
+
+    SelfInputOutput sio = {io, 0};
+    close_pipes(&sio, 0);
+    Message msgs[proc_count + 1];
+    receive_all(&sio, msgs, STARTED);
+    fprintf(logfile, log_received_all_started_fmt, 0);
+    fflush(logfile);
+    receive_all(&sio, msgs, DONE);
+    fprintf(logfile, log_received_all_done_fmt, 0);
+    for (int i = 0; i < sio.io.procCount; i++)
+        wait(NULL);
+    fflush(logfile);
 }
 
 int send(void *self, local_id dst, const Message *msg) {
     SelfInputOutput *sio = (SelfInputOutput *) self;
-    if (sio->self == dst) return -1;
+    if (sio->self == dst) {
+        return -1;
+    }
 
     write(sio->io.fds[sio->self][dst][1], msg, sizeof msg->s_header + msg->s_header.s_payload_len);
     return 0;
@@ -129,11 +137,10 @@ int send_multicast(void *self, const Message *msg) {
 int receive(void *self, local_id from, Message *msg) {
     SelfInputOutput *sio = (SelfInputOutput *) self;
     int fd = sio->io.fds[from][sio->self][0];
-    char b[MAX_MESSAGE_LEN];
     while (1) {
         int sum, sum1;
         if ((sum = read(fd, &msg->s_header, sizeof(MessageHeader))) == -1) {
-            usleep(100);
+            usleep(1000);
             continue;
         }
         if (msg->s_header.s_payload_len > 0) {
@@ -154,14 +161,30 @@ int receive_any(void *self, Message *msg) {
     }
 }
 
+void close_pipes(void *self, int proc) {
+    SelfInputOutput *sio = (SelfInputOutput *) self;
+    for (int i = 0; i <= sio->io.procCount; ++i) {
+        for (int j = 0; j <= sio->io.procCount; ++j) {
+            if (i == j) continue;
+            if (proc == i) {
+                close(sio->io.fds[i][j][0]);
+            } else if (proc == j) {
+                close(sio->io.fds[i][j][1]);
+            } else {
+                close(sio->io.fds[i][j][0]);
+                close(sio->io.fds[i][j][1]);
+            }
+        }
+    }
+}
+
 int receive_all(void *self, Message msgs[], MessageType type) {
     SelfInputOutput *sio = (SelfInputOutput *) self;
     for (int i = 1; i <= sio->io.procCount; ++i) {
         if (i == sio->self) continue;
-//        do {
+        do {
             receive(self, i, &msgs[i]);
-            fflush(stdout);
-//        } while (msgs[i].s_header.s_type != type);
+        } while (msgs[i].s_header.s_type != type);
     }
 
     return 0;
